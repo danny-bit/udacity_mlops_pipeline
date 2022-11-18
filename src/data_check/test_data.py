cc2644280fc5737858cb3fcf4a9c2296e565d2c2
Daniel G <dan.gerstgrasser@tum.de>
2022-11-18 08:18:00 +0100

    completion of all pipeline steps; update of config with optimal random forest parameters

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,6 +60,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 
-########################################################
-# Implement here test_row_count and test_price_range   #
-########################################################
+def test_price_range(data: pd.DataFrame, min_price: float, max_price: float):
+    """
+    Check if the price is premissible range
+    """
+    assert data['price'].between(min_price, max_price).all()
+
+
+def test_row_count(data: pd.DataFrame):
+    """
+    Check the row count 
+    """
+    assert 15000 < data.shape[0] < 1000000
